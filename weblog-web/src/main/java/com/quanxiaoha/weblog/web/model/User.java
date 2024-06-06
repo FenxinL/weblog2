02ce942324abc07afb804a96f754a9becb525f06
13284 <1328414279@qq.com>
2024-06-06 14:12:43 +0800

    Changes

---
+++ weblog-web/src/main/java/com/quanxiaoha/weblog/web/model/User.java
@@ -0,0 +1,17 @@
+package com.quanxiaoha.weblog.web.model;
+
+import lombok.Data;
+
+/**
+ * @author:
+ * @url:
+ * @date:
+ * @description: TODO
+ **/
+@Data
+public class User {
+    // 用户名
+    private String username;
+    // 性别
+    private Integer sex;
+}
